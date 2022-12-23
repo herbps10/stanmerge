49c503ce6f34a229270facef3f4f6e12b46c71f5
Herb Susmann <herbps10@gmail.com>
2022-12-22 21:57:44 -0500

    Automatic reformatting

--- bin/main.ml
+++ bin/main.ml
@@ -2,63 +2,61 @@ open Core_kernel
 open Frontend
 open Transform
 
-
 let name = "%%NAME%%"
 let usage = "Usage: " ^ name ^ " model_file.stan"
-
 let use_config = ref ""
 
 let options =
-  Arg.align [ ( "--config"
-              , Arg.String (fun s -> use_config := s)
-              , "Load configuration file" )
-            ]
+  Arg.align
+    [
+      ( "--config",
+        Arg.String (fun s -> use_config := s),
+        "Load configuration file" );
+    ]
 
 let model_files = ref []
+let add_file filename = model_files := !model_files @ [ filename ]
 
-let add_file filename =
-  model_files := !model_files@[filename]
-
-let get_ast filename = 
+let get_ast filename =
   let res, warnings = Parse.parse_file Parser.Incremental.program filename in
-  Warnings.pp_warnings Fmt.stderr warnings ;
+  Warnings.pp_warnings Fmt.stderr warnings;
   match res with
-    | Result.Ok ast -> ast
-    | Result.Error err ->
-        Errors.pp Fmt.stderr err ;
-        exit 1
+  | Result.Ok ast -> ast
+  | Result.Error err ->
+      Errors.pp Fmt.stderr err;
+      exit 1
 
 let get_ast_from_config (model_file, rules) =
   let ast = get_ast model_file in
-  List.fold rules ~init:ast ~f:(fun acc (x, y) -> Transform.Rename.rename_variable (Str.regexp x) y acc)
+  List.fold rules ~init:ast ~f:(fun acc (x, y) ->
+      Transform.Rename.rename_variable (Str.regexp x) y acc)
 
 type rule = string * string
 type rules = rule list
 type config = (string, rules, String.comparator_witness) Map.t
 
-let parse_rule (name, value) = 
-  match value with
-    | `String s -> (name, s)
-    | _ -> (name, "")
+let parse_rule (name, value) =
+  match value with `String s -> (name, s) | _ -> (name, "")
 
-let parse_rules (name, values) = 
-  let parsed_values = List.map ~f:parse_rule (Yojson.Basic.Util.to_assoc values) in
+let parse_rules (name, values) =
+  let parsed_values =
+    List.map ~f:parse_rule (Yojson.Basic.Util.to_assoc values)
+  in
   (name, parsed_values)
 
-let main () = 
-  Arg.parse options add_file usage ;
-  if (String.compare !use_config "") <> 0 then
+let main () =
+  Arg.parse options add_file usage;
+  if String.compare !use_config "" <> 0 then
     let json = Yojson.Basic.from_file !use_config in
     let config = List.map ~f:parse_rules (Yojson.Basic.Util.to_assoc json) in
     let asts = List.map ~f:get_ast_from_config config in
-    print_endline
-      (Merge.merge_asts asts)
-  else 
+    print_endline (Merge.merge_asts asts)
+  else
     let asts = List.map !model_files ~f:get_ast in
-    let asts_renamed = List.map asts ~f:(Transform.Rename.rename_variable
-    (Str.regexp "var") "alpha") in 
-    print_endline 
-      (Merge.merge_asts asts_renamed)
-    
-  
+    let asts_renamed =
+      List.map asts
+        ~f:(Transform.Rename.rename_variable (Str.regexp "var") "alpha")
+    in
+    print_endline (Merge.merge_asts asts_renamed)
+
 let () = main ()
--- src/transform/Merge.ml
+++ src/transform/Merge.ml
@@ -4,50 +4,67 @@ open Ast
 
 let combine_location_span xloc1 _ = xloc1
 
-let combine_block b1opt b2opt = 
-  match (b1opt, b2opt) with 
-    | (None, None) -> None
-    | (Some b1, None) -> Some b1
-    | (None, Some b2) -> Some b2
-    | (Some b1, Some b2) -> 
-      let { stmts= stmts1; xloc= xloc1 } = b1 in
-      let { stmts= stmts2; xloc= xloc2 } = b2 in
-      let b = { stmts= stmts1@stmts2; xloc= (combine_location_span xloc1 xloc2) } in
+let combine_block b1opt b2opt =
+  match (b1opt, b2opt) with
+  | None, None -> None
+  | Some b1, None -> Some b1
+  | None, Some b2 -> Some b2
+  | Some b1, Some b2 ->
+      let { stmts = stmts1; xloc = xloc1 } = b1 in
+      let { stmts = stmts2; xloc = xloc2 } = b2 in
+      let b =
+        { stmts = stmts1 @ stmts2; xloc = combine_location_span xloc1 xloc2 }
+      in
       Some b
 
 (* For now, comments are stripped out *)
 let combine_comment _ _ = []
 
-let merge_programs (p1 : Ast.untyped_program) (p2 : Ast.untyped_program) = 
-  let { functionblock= bf1
-      ; datablock= bd1
-      ; transformeddatablock= btd1
-      ; parametersblock= bp1
-      ; transformedparametersblock= btp1
-      ; modelblock= bm1
-      ; generatedquantitiesblock= bgq1
-      ; comments = c1 } = p1 in
-  let { functionblock= bf2
-      ; datablock= bd2
-      ; transformeddatablock= btd2
-      ; parametersblock= bp2
-      ; transformedparametersblock= btp2
-      ; modelblock= bm2
-      ; generatedquantitiesblock= bgq2
-      ; comments = c2 } = p2 in
-  let pnew = { 
-      functionblock= (combine_block bf1 bf2)
-    ; datablock= (combine_block bd1 bd2)
-    ; transformeddatablock= (combine_block btd1 btd2)
-    ; parametersblock= (combine_block bp1 bp2)
-    ; transformedparametersblock= (combine_block btp1 btp2)
-    ; modelblock= (combine_block bm1 bm2)
-    ; generatedquantitiesblock= (combine_block bgq1 bgq2)
-    ; comments = (combine_comment c1 c2) } in
+let merge_programs (p1 : Ast.untyped_program) (p2 : Ast.untyped_program) =
+  let {
+    functionblock = bf1;
+    datablock = bd1;
+    transformeddatablock = btd1;
+    parametersblock = bp1;
+    transformedparametersblock = btp1;
+    modelblock = bm1;
+    generatedquantitiesblock = bgq1;
+    comments = c1;
+  } =
+    p1
+  in
+  let {
+    functionblock = bf2;
+    datablock = bd2;
+    transformeddatablock = btd2;
+    parametersblock = bp2;
+    transformedparametersblock = btp2;
+    modelblock = bm2;
+    generatedquantitiesblock = bgq2;
+    comments = c2;
+  } =
+    p2
+  in
+  let pnew =
+    {
+      functionblock = combine_block bf1 bf2;
+      datablock = combine_block bd1 bd2;
+      transformeddatablock = combine_block btd1 btd2;
+      parametersblock = combine_block bp1 bp2;
+      transformedparametersblock = combine_block btp1 btp2;
+      modelblock = combine_block bm1 bm2;
+      generatedquantitiesblock = combine_block bgq1 bgq2;
+      comments = combine_comment c1 c2;
+    }
+  in
   pnew
 
-let merge_asts x = 
-  match x with 
-    [] -> ""
-    | [ast1; ast2] -> Pretty_printing.pretty_print_program (List.fold_left [ast2] ~init:ast1 ~f:merge_programs)
-    | ast::asts -> Pretty_printing.pretty_print_program (List.fold_left asts ~init:ast ~f:merge_programs)
+let merge_asts x =
+  match x with
+  | [] -> ""
+  | [ ast1; ast2 ] ->
+      Pretty_printing.pretty_print_program
+        (List.fold_left [ ast2 ] ~init:ast1 ~f:merge_programs)
+  | ast :: asts ->
+      Pretty_printing.pretty_print_program
+        (List.fold_left asts ~init:ast ~f:merge_programs)
